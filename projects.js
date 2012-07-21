e0e26f627f59290d6398201d854be71aaf74a8c3
Cayenne <cayennes@gmail.com>
2012-07-21 14:52:57 -0400

    Create showcase page
    This page shows projects based on a URL parameter

--- projects.js
+++ projects.js
@@ -1,39 +1,84 @@
 /*jslint plusplus: true, maxlen: 72, browser: true */
 /*globals $ */
 
-var RavelryThing = {
+var cyRavelryThing;
+
+// Singleton object to manage the projects
+cyRavelryThing = {
     projects: [],
 
     loadData: function (data) {
         "use strict";
-        this.projects = data.projects;
+        var Project, i;
+        // Project class
+        Project = function (ravelryProject) {
+            var key;
+            for (key in ravelryProject) {
+                if (ravelryProject.hasOwnProperty(key)) {
+                    this[key] = ravelryProject[key];
+                }
+            }
+        };
+        Project.prototype.getLinkImgHtml = function () {
+            var html = '';
+            if (this.thumbnail) {
+                html = '<a href="' + this.url + '">' +
+                    '<img ' + 'src="' + this.thumbnail.src + '" ' +
+                    'alt="' + this.name + '" /></a>';
+            }
+            return html;
+        };
+
+        // Load the data
+        for (i = data.projects.length - 1; i >= 0; i--) {
+            this.projects[i] = new Project(data.projects[i]);
+        }
     },
 
-    getHappiest: function () {
+    getProjects: function (filters) {
         "use strict";
-        var i, j,
-            happyProjects = [];
-        for (i = this.projects.length - 1; i--; i >= 0) {
-            if (this.projects[i].happiness === 4) {
-                happyProjects.push(this.projects[i]);
+        var i, key,
+            selectedProjects = this.projects.slice(0);
+        for (i = selectedProjects.length - 1; i >= 0; i--) {
+            for (key in filters) {
+                if (filters.hasOwnProperty(key)) {
+                    if ($.inArray(
+                            selectedProjects[i][key],
+                            filters[key]
+                        ) === -1) {
+                        selectedProjects.splice(i, 1);
+                        break;
+                    }
+                }
             }
         }
-        return happyProjects;
+        return selectedProjects;
+    },
+
+    getGallery: function (filters) {
+        "use strict";
+        var i,
+            html = '',
+            projects = this.getProjects(filters);
+        for (i = projects.length - 1; i >= 0; i--) {
+            html += projects[i].getLinkImgHtml();
+        }
+        return html;
     }
 };
 
+// Place the projects on the page
 $(document).ready(function () {
     "use strict";
-    var i, happiest, happiestHtml,
-        $happiestProjectDiv = $('#happiest');
+    var i, filter, projects,
+        $happiestProjectDiv = $('#happiest'),
+        $selectedProjectDiv = $('#selected');
     if ($happiestProjectDiv.length) {
-        happiest = RavelryThing.getHappiest();
-        happiestHtml = '';
-        for (i = happiest.length - 1; i >= 0; i--) {
-            happiestHtml += '<a href="' + happiest[i].url + '"><img ' +
-                'src="' + happiest[i].thumbnail.src + '" ' +
-                 'alt="' + happiest[i].name + '" /></a>';
-        }
-        $happiestProjectDiv.html(happiestHtml);
+        filter = {'happiness': [4]};
+        $happiestProjectDiv.html(cyRavelryThing.getGallery(filter));
+    }
+    if ($selectedProjectDiv.length) {
+        filter = {'permalink': $.url().param('projects').split('+')};
+        $selectedProjectDiv.html(cyRavelryThing.getGallery(filter));
     }
 });
---
+++ yarncraft/showcase.markdown
@@ -0,0 +1,9 @@
+---
+layout: page
+title: Project Showcase
+linkbar: <div class="leftbar" id="menu0"><a href="index.html">Home</a> | <a href="patterns/index.html">Patterns</a> | <a href="combination.html">Combination Knitting</a></div>
+---
+Each image is a link to the ravelry page with more details.
+
+<div class="projects" id="selected">If you have JavaScript
+enabled, the projects will appear here in a moment.</div>
